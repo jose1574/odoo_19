b87b65e610c514247c22e91e97a6f8f221bf7cc3
jose1574 <jose.gs1574@gmail.com>
2026-05-02 15:28:06 -0400

    primer commit

---
+++ customs_addons/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Modulos personalizados Goca',
+    'version': '1.0',
+    'category': 'Extra Tools',
+    'summary': 'modulos y personalizaciones para goca',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': True,
+}
